7c09a1de5092d6c1f426fc25b1a7b0b69b44a0a6
Silver CHEN <silver.sojo@gmail.com>
2021-05-28 20:34:51 +0800

    Fri 28 May 2021 08:34:51 PM CST

--- includes/config.php
+++ includes/config.php
@@ -6,10 +6,10 @@ error_reporting(E_ALL);
 ini_set('display_errors','On');
 
 // bitcoin rpc info
-define('USER','xxxx');
-define('PASS','xxxx');
-define('SERVER','xxxx');
-define('PORT','xxxx');
+define('USER','admin');
+define('PASS','1234');
+define('SERVER','192.168.1.102');
+define('PORT','8332');
 
 #define('SSL_CERT_PATH','/var/www/coin/includes/server.cert'); # probably best if this is a hardcoded full path
 
